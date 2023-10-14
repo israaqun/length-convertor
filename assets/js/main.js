21adcd0a02f947cdcb1ef4b1c7733fe8ec7ef3e4
israaqun <israaali848@gmail.com>
2023-10-14 16:31:11 +0300

    Update development branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,38 @@
-const units=["meter","inch","cm","km","foot","yard"],conversions={meter:1,inch:39.3701,cm:100,km:.001,foot:3.28084,yard:1.09361};function convertLength(e,t){const o=parseFloat(e.value);isNaN(o)?(e.value="",alert("Please enter a valid number.")):units.forEach(e=>{var n;e!==t&&(n=o/(conversions[t]/conversions[e]),document.getElementById(e).value=n.toFixed(3))})}function setupEventListeners(){units.forEach(e=>{const n=document.getElementById(e);n.addEventListener("input",()=>convertLength(n,e))})}setupEventListeners();
+const units = ['meter', 'inch', 'cm', 'km', 'foot', 'yard'];
+
+const conversions = {
+  meter: 1,
+  inch: 39.3701,
+  cm: 100,
+  km: 0.001,
+  foot: 3.28084,
+  yard: 1.09361,
+};
+
+
+function convertLength(input, unit) {
+  const inputValue = parseFloat(input.value);
+
+  if (!isNaN(inputValue)) {
+      units.forEach((otherUnit) => {
+          if (otherUnit !== unit) {
+              const result = inputValue / (conversions[unit] / conversions[otherUnit]);
+              const otherInput = document.getElementById(otherUnit);
+              otherInput.value = result.toFixed(3);
+          }
+      });
+  } else {
+     
+      input.value = ''; 
+      alert('Please enter a valid number.');
+  }
+}
+
+function setupEventListeners() {
+  units.forEach((unit) => {
+    const input = document.getElementById(unit);
+    input.addEventListener('input', () => convertLength(input, unit));
+  });
+}
+
+setupEventListeners();
